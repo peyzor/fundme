6caee5995679c2e5e513b235888ec19715964222
peyzor <hassani.peyman627@gmail.com>
2024-12-26 21:39:13 +0330

    Add onlyOwner modifier

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -11,19 +11,31 @@ contract FundMe {
     address[] funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
+
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "call failed");
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "must be owner");
+        _;
     }
 }
